03d8685bfe56424b5c9bf2850f51c80375eaba59
namsh1125 <namsh1125@naver.com>
2025-02-14 02:19:08 +0900

    ✨ 방 생성 API 연결

--- app/(main)/page.js
+++ app/(main)/page.js
@@ -14,82 +14,98 @@ import { INITIAL_WIDTHS } from "@/constants/panel-config";
  * 코드 에디터와 사이드 패널들을 관리하는 최상위 컴포넌트
  */
 export default function Home() {
-    const router = useRouter();
-    const [showCreateModal, setShowCreateModal] = useState(true);
-    const [code, setCode] = useState(INITIAL_CODE);
+  const router = useRouter();
+  const [showCreateModal, setShowCreateModal] = useState(true);
+  const [code, setCode] = useState(INITIAL_CODE);
 
-    const handleCreateRoom = () => {
-        // TODO: API 호출하여 방 생성
-        const roomId = "fb1dfb89-393f-4e9d-9d62-3b2af9afe788"; // API에서 받아올 실제 UUID
-        router.push(`/${roomId}`);
-    };
+  const handleCreateRoom = async (title, password) => {
+    try {
+      const response = await fetch("/api/rooms", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ title, password }),
+      });
 
-    return (
-        <>
-            {/* 기존 UI를 블러 처리 */}
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(data.error);
+      }
+
+      router.push(`/${data.data.uuid}`);
+    } catch (error) {
+      console.error("방 생성 실패:", error);
+      // TODO: 에러 처리
+    }
+  };
+
+  return (
+    <>
+      {/* 기존 UI를 블러 처리 */}
+      <div
+        className={`${
+          showCreateModal ? "filter blur-sm pointer-events-none" : ""
+        }`}
+      >
+        <div className="flex h-[calc(100vh-64px)]">
+          {/* 좌측 아이콘 사이드바 */}
+          <div className="w-12 h-full bg-gray-900 border-r border-gray-800 flex-shrink-0">
+            <div className="flex flex-col items-center py-4">
+              <button className="p-3 text-gray-400" disabled>
+                <FaHistory size={18} />
+              </button>
+            </div>
+          </div>
+
+          {/* 메인 컨텐츠 (코드 에디터) */}
+          <div className="flex-1">
+            <div className="p-2 h-full">
+              <CodeEditor
+                code={code}
+                onCodeChange={setCode}
+                isDisabled={showCreateModal}
+              />
+            </div>
+          </div>
+
+          {/* 우측 패널 영역 */}
+          <div className="relative flex">
+            {/* Questions 패널 */}
             <div
-                className={`${
-                    showCreateModal ? "filter blur-sm pointer-events-none" : ""
-                }`}
+              className="bg-gray-900 border-l border-gray-800"
+              style={{ width: `${INITIAL_WIDTHS.RIGHT}px` }}
             >
-                <div className="flex h-[calc(100vh-64px)]">
-                    {/* 좌측 아이콘 사이드바 */}
-                    <div className="w-12 h-full bg-gray-900 border-r border-gray-800 flex-shrink-0">
-                        <div className="flex flex-col items-center py-4">
-                            <button className="p-3 text-gray-400" disabled>
-                                <FaHistory size={18} />
-                            </button>
-                        </div>
-                    </div>
-
-                    {/* 메인 컨텐츠 (코드 에디터) */}
-                    <div className="flex-1">
-                        <div className="p-2 h-full">
-                            <CodeEditor
-                                code={code}
-                                onCodeChange={setCode}
-                                isDisabled={showCreateModal}
-                            />
-                        </div>
-                    </div>
-
-                    {/* 우측 패널 영역 */}
-                    <div className="relative flex">
-                        {/* Questions 패널 */}
-                        <div
-                            className="bg-gray-900 border-l border-gray-800"
-                            style={{ width: `${INITIAL_WIDTHS.RIGHT}px` }}
-                        >
-                            <div className="p-4 h-full">
-                                <QuestionsPanel />
-                            </div>
-                        </div>
-
-                        {/* 우측 아이콘 사이드바 */}
-                        <div className="w-12 bg-gray-900 border-l border-gray-800">
-                            <div className="flex flex-col items-center py-4">
-                                <button
-                                    className="p-3 mb-2 text-blue-400 bg-blue-500/20"
-                                    disabled
-                                >
-                                    <FaQuestion size={18} />
-                                </button>
-                                <button className="p-3 text-gray-400" disabled>
-                                    <FaVoteYea size={18} />
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+              <div className="p-4 h-full">
+                <QuestionsPanel />
+              </div>
             </div>
 
-            {/* 방 생성 모달 */}
-            <RoomCreateModal
-                isOpen={showCreateModal}
-                onClose={() => setShowCreateModal(false)}
-                onSubmit={handleCreateRoom}
-            />
-        </>
-    );
-}
+            {/* 우측 아이콘 사이드바 */}
+            <div className="w-12 bg-gray-900 border-l border-gray-800">
+              <div className="flex flex-col items-center py-4">
+                <button
+                  className="p-3 mb-2 text-blue-400 bg-blue-500/20"
+                  disabled
+                >
+                  <FaQuestion size={18} />
+                </button>
+                <button className="p-3 text-gray-400" disabled>
+                  <FaVoteYea size={18} />
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
+      {/* 방 생성 모달 */}
+      <RoomCreateModal
+        isOpen={showCreateModal}
+        onClose={() => setShowCreateModal(false)}
+        onSubmit={handleCreateRoom}
+      />
+    </>
+  );
+}
