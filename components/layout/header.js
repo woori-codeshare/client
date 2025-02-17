99fa8f611fdd00d1ecf993e2389e24f68851d723
namsh1125 <namsh1125@naver.com>
2025-02-17 19:58:48 +0900

    💄 헤더 및 favicon 디자인 수정

--- components/layout/header.js
+++ components/layout/header.js
@@ -10,14 +10,24 @@ export default function Header() {
   return (
     <header className="border-b border-gray-800 p-4">
       <div className="flex justify-between items-center pl-4">
-        <Image
-          src="/img/woori-codeshare.png"
-          alt="CodeShare Logo"
-          width={150}
-          height={40}
-          priority
-          className="text-2xl font-bold"
-        />
+        <div className="flex items-center gap-4">
+          <Image
+            src="/img/logo.png"
+            alt="Woori CodeShare Logo"
+            width={40}
+            height={40}
+            priority
+            className="rounded-full"
+          />
+          <Image
+            src="/img/woori-codeshare.png"
+            alt="CodeShare Logo"
+            width={200}
+            height={50}
+            priority
+            className="text-2xl font-bold"
+          />
+        </div>
         <div className="flex items-center gap-4">
           <button className="relative p-2">
             {/* TODO: 추후 알림 기능을 구현할 때 주석 해제 */}
