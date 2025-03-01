4308fa9a53bb9ca57c5e578686e322a89615ceff
namsh1125 <namsh1125@naver.com>
2025-03-02 08:34:47 +0900

    ✨ Google Analytics, Clarity 설정 추가

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,31 @@ export default function RootLayout({ children }) {
     <html lang="ko" suppressHydrationWarning className="overflow-hidden">
       <head>
         <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LRSD0EJFQG"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LRSD0EJFQG');
+            `,
+          }}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qgurtqpcgg");
+            `,
+          }}
+        />
+        <script
           dangerouslySetInnerHTML={{
             __html: `
               try {
